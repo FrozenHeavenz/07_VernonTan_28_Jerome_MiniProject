d4426e6b07f745e1b00042dbe0630dfa024c1898
FrozenHeavenz <64124272+FrozenHeavenz@users.noreply.github.com>
2020-08-13 16:48:55 +0800

    Updated Game

---
+++ Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+    public GameObject[] SpawnerX;
+    public GameObject[] SpawnerNegX;
+    public GameObject[] SpawnerY;
+    public GameObject[] SpawnerNegY;
+
+    public GameObject SelectedSpawnerX;
+    public GameObject SelectedSpawnerNegX;
+    public GameObject SelectedSpawnerY;
+    public GameObject SelectedSpawnerNegY;
+
+    public int SRandX;
+    public int SRandNegX;
+    public int SRandY;
+    public int SRandNegY;
+
+    public GameObject[] Enemies;
+
+    public int ERand;
+
+    public GameObject SelectedEnemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnerX = GameObject.FindGameObjectsWithTag("SpawnerX");
+        SpawnerNegX = GameObject.FindGameObjectsWithTag("Spawner-X");
+        SpawnerY = GameObject.FindGameObjectsWithTag("SpawnerY");
+        SpawnerNegY = GameObject.FindGameObjectsWithTag("Spawner-Y");
+
+
+        InvokeRepeating("PositiveX", 1, 1.5f);
+        InvokeRepeating("NegativeX", 1, 1.5f);
+        InvokeRepeating("PositiveY", 1, 1.5f);
+        InvokeRepeating("NegativeY", 1, 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PositiveX()
+    {
+        SRandX = Random.Range(0, 4);
+        ERand = Random.Range(0, 3);
+
+        SelectedEnemy = Enemies[ERand];
+        SelectedSpawnerX = SpawnerX[SRandX];
+
+        Instantiate(SelectedEnemy, SelectedSpawnerX.transform.position, Quaternion.identity);
+
+    }
+
+    public void NegativeX()
+    {
+        SRandNegX = Random.Range(0, 4);
+        ERand = Random.Range(0, 3);
+
+        SelectedEnemy = Enemies[ERand];
+        SelectedSpawnerNegX = SpawnerNegX[SRandNegX];
+
+        Instantiate(SelectedEnemy, SelectedSpawnerNegX.transform.position, Quaternion.identity);
+    }
+
+    public void PositiveY()
+    {
+        SRandY = Random.Range(0, 9);
+        ERand = Random.Range(0, 3);
+
+        SelectedEnemy = Enemies[ERand];
+        SelectedSpawnerY = SpawnerY[SRandY];
+
+        Instantiate(SelectedEnemy, SelectedSpawnerY.transform.position, Quaternion.identity);
+    }
+
+    public void NegativeY()
+    {
+        SRandNegY = Random.Range(0, 9);
+        ERand = Random.Range(0, 3);
+
+        SelectedEnemy = Enemies[ERand];
+        SelectedSpawnerNegY = SpawnerNegY[SRandNegY];
+
+        Instantiate(SelectedEnemy, SelectedSpawnerNegY.transform.position, Quaternion.identity);
+    }
+}
